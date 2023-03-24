1476ba2d717a57f5e58d23eea1f2d49560f886c2
pochun.huang1107@gmail.com <casper@Po-Chuns-MacBook-Pro.local>
2023-03-24 15:57:56 +0800

    final commit

---
+++ src/store/slices/formSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { addCar } from "./carsSlice";
+
+const formSlice = createSlice({
+    name: 'form',
+    initialState: {
+        name: '',
+        cost: 0
+    },
+    reducers: {
+        changeName(state, action) {
+            state.name = action.payload;
+        },
+        changeCost(state, action) {
+            state.cost = action.payload;
+        }
+    },
+    extraReducers(builder){
+        builder.addCase(addCar, () => 
+            formSlice.getInitialState()
+        );
+    }
+});
+
+export const { changeName, changeCost } = formSlice.actions;
+export const formReducer = formSlice.reducer;
